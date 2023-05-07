93f705111af0b96a3b64e56c58ae563d20545c3b
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-07 17:14:27 -0600

    get all posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
+import { PostsService } from './posts/posts.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule],
+  controllers: [PostsController],
+  providers: [PostsService],
 })
 export class AppModule {}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Posts } from './posts.entity';
+
+@Injectable()
+export class PostsService {
+  // simulate a database
+  private posts: Posts[] = [
+    {
+      id: 1,
+      title: 'First post',
+      content: 'This is the first post',
+    },
+    {
+      id: 2,
+      title: 'Second post',
+      content: 'This is the second post',
+    },
+  ];
+
+  getPosts() {
+    return this.posts;
+  }
+}
