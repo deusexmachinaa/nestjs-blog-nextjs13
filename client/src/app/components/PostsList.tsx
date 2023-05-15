6a6b6aa3e3f23e398c8e81f5530541ae5cf3e05e
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-14 20:16:32 -0600

    trying to use the /posts/ from the main page

---
+++ client/src/app/components/PostsList.tsx
@@ -0,0 +1,17 @@
+import { getPosts } from '../../api/post.api';
+import { useEffect, useState } from 'react';
+
+function index() {
+  useEffect(() => {
+    async function getAllPosts() {
+      const res = await getPosts;
+      console.log(res);
+    }
+
+    getAllPosts;
+  }, []);
+
+  return <div>index</div>;
+}
+
+export default index;
