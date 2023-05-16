a1c9720bb340104bb91fe057b73596e9e88c7ea5
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-15 20:02:30 -0600

    fixing some errors

--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -1,19 +1,42 @@
 import { getPosts } from '../api/post.api';
 import { useEffect, useState } from 'react';
+import { Posts } from '../../types/posts.index';
 
 function PostsList() {
-  const [posts, setposts] = useState();
+  const [posts, setposts] = useState<Posts[]>([]);
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     const getAllPosts = async () => {
-      const res = await getPosts;
-      console.log(res);
+      try {
+        const res = await getPosts();
+        const { data } = res;
+        setposts(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setloading(false);
+      }
     };
 
     getAllPosts();
   }, []);
 
-  return <div>index</div>;
+  return (
+    <div>
+      <h1>Posts</h1>
+      {loading ? (
+        <p>Loading posts...</p>
+      ) : (
+        posts.map((post: Posts) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default PostsList;
