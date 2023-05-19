ae4b2961f6b86d9007006973b12425d454c78f06
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-18 20:12:03 -0600

    posts props fixed

--- client/src/components/posts/PostCard.tsx
+++ client/src/components/posts/PostCard.tsx
@@ -1,9 +1,10 @@
 import { Posts } from '../../../types/posts.index';
 
-function PostCard({ post }: Posts) {
+function PostCard({ post }: { post: Posts }) {
   return (
     <div>
       <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </div>
   );
 }
