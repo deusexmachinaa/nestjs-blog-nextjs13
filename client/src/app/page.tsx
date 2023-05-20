da97f559afcaffa166ff128270b460fe390d715a
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-19 20:39:22 -0600

    example "/" page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function page() {
   return (
-    <div>Home page</div>
-  )
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta nemo
+        corrupti omnis perspiciatis! Tempore sunt, reiciendis sed distinctio
+        maxime dignissimos ullam fugit. Esse quidem facilis tempore alias
+        suscipit. Quidem qui beatae accusantium odit facere quasi, soluta sequi,
+        obcaecati quae eos libero iste blanditiis cupiditate nemo dicta voluptas
+        nesciunt magnam delectus.
+      </p>
+    </div>
+  );
 }
 
-export default page
+export default page;
